112431828bbfbf8ff05af8cb13575a505bd76d07
Jeremy Brown <jeremy@notmessenger.com>
2024-10-20 21:57:10 -0500

    Overlooked importing `createStreamingMessage`

--- anthropic/service.ts
+++ anthropic/service.ts
@@ -1,5 +1,5 @@
 import Restack, { ServiceInput } from "@restackio/ai";
-import { createNonStreamingMessage } from "./functions";
+import { createNonStreamingMessage, createStreamingMessage } from "./functions";
 import { anthropicTaskQueue } from "./taskQueue";
 
 export async function anthropicService({
@@ -11,7 +11,7 @@ export async function anthropicService({
 }) {
   await client.startService({
     taskQueue: anthropicTaskQueue,
-    functions: { createNonStreamingMessage },
+    functions: { createNonStreamingMessage, createStreamingMessage },
     options,
   });
 }
